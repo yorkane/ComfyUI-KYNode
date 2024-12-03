ae44bf2ee6cd5c7b63f00a80f35a7059ac411f90
Kevin Yuan <whyork@qq.com>
2024-12-03 15:08:06 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,19 +2,15 @@ from .advanced_lying_sigma_sampler import AdvancedLyingSigmaSamplerNode
 from .KY_SaveImageToPath import KY_SaveImageToPath
 from .openai_VLM import OpenAICaptionImage
 
-NODE_CLASS_MAPPINGS = {
-    "OpenAI.CaptionImage": OpenAICaptionImage
-}
-
 # 将节点映射到NODE_CLASS_MAPPINGS和NODE_DISPLAY_NAME_MAPPINGS
 NODE_CLASS_MAPPINGS = {
     "AdvancedLyingSigmaSampler": AdvancedLyingSigmaSamplerNode,
     "KY_SaveImageToPath": KY_SaveImageToPath,
-    "OpenAICaptionImage": KY_OpenAICaptionImage
+    "KY_OpenAICaptionImage": OpenAICaptionImage
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "AdvancedLyingSigmaSampler": "Advanced Lying Sigma Sampler",
     "KY_SaveImageToPath": "Save Image To target Path",
-    "KY_OpenAICaptionImage": "Describe Image from openai-protocol"
+    "KY_OpenAICaptionImage": "KY Describe Image from openai-protocol"
 }
