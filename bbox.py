6e8cff27f0402526f974e1522dff14d7aadfbb2f
yorkane <whyork@qq.com>
2025-09-01 23:15:47 +0800

    add create mask node

--- bbox.py
+++ bbox.py
@@ -13,6 +13,8 @@ import torch
 from torch import Tensor
 from torchvision import ops
 from torchvision.transforms import functional
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
+from .utils.utility import pil2tensor, tensor2pil
 
 _CATEGORY = "KYNode/BBox"
 
@@ -229,6 +231,77 @@ class ImageCropByBBox:
             pass
         return (results,)
     
+class CreateShapeMask:
+    
+    RETURN_TYPES = ("MASK", "MASK",)
+    RETURN_NAMES = ("mask", "mask_inverted",)
+    FUNCTION = "createshapemask"
+    CATEGORY = "KJNodes/masking/generate"
+    DESCRIPTION = """
+Creates a mask or batch of masks with the specified shape.  
+Locations are center locations.  
+Grow value is the amount to grow the shape on each frame, creating animated masks.
+"""
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "shape": (
+            [   'circle',
+                'square',
+                'triangle',
+            ],
+            {
+            "default": 'circle'
+             }),
+                "frames": ("INT", {"default": 1,"min": 1, "max": 4096, "step": 1}),
+                "location_x": ("INT", {"default": 256,"min": 0, "max": 4096, "step": 1}),
+                "location_y": ("INT", {"default": 256,"min": 0, "max": 4096, "step": 1}),
+                "grow": ("INT", {"default": 0, "min": -512, "max": 512, "step": 1}),
+                "frame_width": ("INT", {"default": 512,"min": 16, "max": 4096, "step": 1}),
+                "frame_height": ("INT", {"default": 512,"min": 16, "max": 4096, "step": 1}),
+                "shape_width": ("INT", {"default": 128,"min": 8, "max": 4096, "step": 1}),
+                "shape_height": ("INT", {"default": 128,"min": 8, "max": 4096, "step": 1}),
+        },
+    } 
+
+    def createshapemask(self, frames, frame_width, frame_height, location_x, location_y, shape_width, shape_height, grow, shape):
+        # Define the number of images in the batch
+        batch_size = frames
+        out = []
+        color = "white"
+        for i in range(batch_size):
+            image = Image.new("RGB", (frame_width, frame_height), "black")
+            draw = ImageDraw.Draw(image)
+
+            # Calculate the size for this frame and ensure it's not less than 0
+            current_width = max(0, shape_width + i*grow)
+            current_height = max(0, shape_height + i*grow)
+
+            if shape == 'circle' or shape == 'square':
+                # Define the bounding box for the shape
+                left_up_point = (location_x, location_y) # bottom left
+                right_down_point = (location_x + current_width, location_y + current_height) # bottom right
+                two_points = [left_up_point, right_down_point]
+
+                if shape == 'circle':
+                    draw.ellipse(two_points, fill=color)
+                elif shape == 'square':
+                    draw.rectangle(two_points, fill=color)
+                    
+            elif shape == 'triangle':
+                # Define the points for the triangle
+                left_up_point = (location_x - current_width // 2, location_y + current_height // 2) # bottom left
+                right_down_point = (location_x + current_width // 2, location_y + current_height // 2) # bottom right
+                top_point = (location_x, location_y - current_height // 2) # top point
+                draw.polygon([top_point, left_up_point, right_down_point], fill=color)
+
+            image = pil2tensor(image)
+            mask = image[:, :, :, 0]
+            out.append(mask)
+        outstack = torch.cat(out, dim=0)
+        return (outstack, 1.0 - outstack,)
 
 BBOX_NODE_CLASS_MAPPINGS = {
     "KY_BBoxesToSAM2": BBoxesToSAM2,
@@ -236,6 +309,8 @@ BBOX_NODE_CLASS_MAPPINGS = {
     "KY_toBBox": toBBox,
     "KY_JSONToBBox": JSONToBBox,
     "KY_ImageCropByBBox": ImageCropByBBox,
+    "KY_CreateMask": CreateShapeMask,
+    
 }
 
 BBOX_NODE_NAME_MAPPINGS = {
@@ -244,4 +319,5 @@ BBOX_NODE_NAME_MAPPINGS = {
     "KY_toBBox": "Convert X Y WH into box",
     "KY_JSONToBBox": "json string to bbox",
     "KY_ImageCropByBBox": "Crop Image by bbox",
+    "KY_CreateMask": "Crop Mask by xywh",
 }
